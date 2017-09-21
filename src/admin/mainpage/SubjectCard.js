7c8b309389ba217eaa357e7a95674df7a2ac217d
Panyawut Sri-iesaranusorn <panyawutsri-iesaranusorn@MacBook-Pro-khxng-Panyawut.local>
2017-09-21 19:27:43 +0700

    add SubjectCard in admin mainpage

---
+++ src/admin/mainpage/SubjectCard.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react'
+
+
+class SubjectCard extends Component{
+    constructor(porps){
+        super(props)
+        this.state = {
+            name:"test1",
+            comment:"test",
+            
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default SubjectCard
